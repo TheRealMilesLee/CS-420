e0b6626e6c0037442f7db68857e4f612b0170ad7
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-08-31 11:48:00 -0500

    Merge remote-tracking branch 'refs/remotes/origin/

--- Week1-SimpleCompiler/Class_Example/macOS_Version/simpleCompiler.c
+++ Week1-SimpleCompiler/Class_Example/macOS_Version/simpleCompiler.c
@@ -1,36 +1,37 @@
+/**
+ * @file simpleCompiler.c
+ * @author Hengyi Li (hl3265@truman.edu) and Dr.Yu (cyyu@truman.edu)
+ * @brief This file is to show a simple compiler that compile an integer
+ * @version 0.1
+ * @date 2022-08-31
+ * @copyright Copyright (c) 2022. Truman State University. All rights reserve
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+  // If the parameter is empty, throw error
   if (argc != 2)
   {
     fprintf(stderr, "Num of args is wrong");
     return 1;
   }
-  char *p = argv[1];
+
+  // Read in a number from user and cast to integer
+  int input_integer = atoi(argv[1]);
   printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
-
-  while (*p)
-  {
-    if (*p == '+')
-    {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    if (*p == '-')
-    {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    fprintf(stderr, "unexpected chars: '%c'\n", *p);
-  }
+  /**
+   * note: I add underline symbol before main so that would compile as normal
+   * It might related to some system architecture issue, but Unix machine all
+   * need this underline symbol to let gcc finished compile  .s file into
+   * .target file, otherwise it would report error "implicit entry/start for
+   * main executable"
+   */
+  printf(".global _main\n");
+  printf("_main:\n");
+  printf("  mov rax, %d\n", input_integer);
   printf("  ret\n");
   return 0;
 }

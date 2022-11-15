e4f9ff1f9bab61d7bfe31c34f566ef50c2081ce6
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-11-14 23:04:10 -0600

    Add the documentation

--- Homework-5/PersonalExperiment/Scanner.c
+++ Homework-5/PersonalExperiment/Scanner.c
@@ -1,19 +1,32 @@
+/**
+ * @file Scanner.c
+ * @author Hengyi Li and Dr.Yu
+ * @brief This file is the lexical scanner
+ * @version 0.1
+ * @date 2022-11-14
+ * @copyright Copyright (c) 2022. Hengyi Li, All rights reserved
+ */
 #include "Scanner.h"
 #include <stdio.h>
 
+/* A declaration of the functions and variables defined in the lexical analyzer. */
 extern int yylex();
 extern int yylineno;
 extern char *yytext;
 
-char *names[] = {NULL, "Listen", "ServerName", "LogLevel", "PHPIniDir"};
+/* A global variable that is used to store the names of the tokens. */
+char *names[] = {NULL, "LISTEN_PORT", "SERVER_NAME", "LOG_LEVEL", "PHP_INI_DIR"};
 
 int main(void)
 {
+ /* Getting the token from the lexical analyzer. */
   int name_token, value_token;
   name_token = yylex();
+ /* Checking if the token is valid. */
   while (name_token)
   {
-    printf("%d\n", name_token);
+  /* Checking if the token is valid. */
+    printf("name token is %d\n", name_token);
     if (yylex() != COLON)
     {
       printf("Syntax error in line %d, Expected a ':' but found %s\n", yylineno,
@@ -21,6 +34,7 @@ int main(void)
       return 1;
     }
     value_token = yylex();
+  /* Checking if the value token is an integer. */
     switch (name_token)
     {
     case LISTEN_PORT:
@@ -32,6 +46,7 @@ int main(void)
       }
       printf("%s is set to %s\n", names[name_token], yytext);
       break;
+    /* Checking if the value token is an identifier. */
     case SERVER_NAME:
     case LOG_LEVEL:
     case PHP_INI_DIR:
@@ -43,9 +58,11 @@ int main(void)
       }
       printf("%s is set to %s\n", names[name_token], yytext);
       break;
+  /* A default case for the switch statement. */
     default:
       printf("Syntax error in line %d\n", yylineno);
     }
+/* Getting the token from the lexical analyzer. */
     name_token = yylex();
   }
   return 0;
